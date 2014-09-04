22473da25152556da8981a2e60a2bb45e398ad72
vagrant <vagrant@precise32.(none)>
2014-09-04 09:38:21 +0000

    fix bug.

--- ladymarry/settings.py
+++ ladymarry/settings.py
@@ -8,7 +8,7 @@ SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost:3306/ladymarry'
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
 # JWT configs.
-JWT_AUTH_URL_RULE = '/api/auth'
+JWT_AUTH_URL_RULE = '/auth'
 JWT_EXPIRATION_DELTA = timedelta(days=7)
 
 # Data configs.
