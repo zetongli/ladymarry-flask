b44a8ed12b62b567c4c9562da7cd0140fff43b1a
vagrant <vagrant@precise32.(none)>
2014-10-21 01:17:20 +0000

    Make token expiration time to 3 months.

--- ladymarry/settings.py
+++ ladymarry/settings.py
@@ -9,7 +9,7 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
 # JWT configs.
 JWT_AUTH_URL_RULE = '/auth'
-JWT_EXPIRATION_DELTA = timedelta(days=7)
+JWT_EXPIRATION_DELTA = timedelta(days=90)
 
 # Data configs.
 TASK_DATA_FILE = './ladymarry/data/task_data.csv'
